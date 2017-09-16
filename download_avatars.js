c48f6132e0735f28ab563239829b6df9ba41ae85
Andrew Park <andresparque@gmail.com>
2017-09-16 20:19:02 +0000

    First update

---
+++ download_avatars.js
@@ -0,0 +1,9 @@
+var request = require('request');
+
+console.log('Welcome to the GitHub Avatar Downloader!');
+
+// function getRepoContributors(repoOwner, repoName, cb) {
+//   // ...
+
+//   https.Get('', function(error, body, callback))
+// }
